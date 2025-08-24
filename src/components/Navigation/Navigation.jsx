20e9fdbfb7e50a795e0f2490b316fba5bb4f7405
YashodMahela <hmymahela78@gmail.com>
2025-08-24 20:17:48 +0530

    Add new chart page and signIn/Out pages

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,7 +1,14 @@
 import { Link } from "react-router";
 import styles from "./Navigation.module.css";
-
+import { Menu, X, ShoppingBag, Search, User } from "lucide-react";
+import { SignedIn, UserButton, SignedOut } from "@clerk/clerk-react";
+import { useSelector } from "react-redux";
 function Navigation() {
+  const cartItems = useSelector((state) => state.cart.cartItems);
+  const cartItemCount = cartItems.reduce(
+    (total, item) => total + item.quantity,
+    0
+  );
   return (
     <header className={styles.navbar}>
       <Link to="/" className={styles.logo}>Mebius</Link>
@@ -26,26 +33,27 @@ function Navigation() {
             <path d="m21 21-4.3-4.3" />
           </svg>
         </div>
-        <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="lucide lucide-shopping-bag-icon lucide-shopping-bag"
-          >
-            <path d="M6 2 3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4Z" />
-            <path d="M3 6h18" />
-            <path d="M16 10a4 4 0 0 1-8 0" />
-          </svg>
+        <Link
+              to="/shop/cart"
+              aria-label="Shopping Bag"
+              className="p-1 relative mx-4"
+            >
+              <ShoppingBag size={20} />
+              <span className="absolute -top-1 -right-1 bg-black text-white text-xs w-4 h-4 flex items-center justify-center rounded-full">
+                {cartItemCount}
+              </span>
+            </Link>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <div className="hidden md:block">
+          <SignedOut>
+            <div className="flex items-center gap-4">
+              <Link to="/sign-in">Sign In</Link>
+              <Link to="/sign-up">Sign Up</Link>
+            </div>
+          </SignedOut>
         </div>
-        <a href="">Sign In</a>
-        <a href="">Sign Up</a>
       </div>
     </header>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -8,21 +8,35 @@ import { store } from "./lib/store.js";
 import { Provider } from "react-redux";
 import ShopPage from "./pages/shop.page.jsx";
 import ProductPage from "./pages/product.page.jsx";
+import SignUpPage from "./sign-up.page.jsx";
+import SignInPage from "./sign-in.page.jsx";
+import CartPage from "./pages/cart.page.jsx";
+import { ClerkProvider } from "@clerk/clerk-react";
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<RootLayout />}>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/shop" element={<ShopPage />}>
-              <Route path=":category" element={<ShopPage />} />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<RootLayout />}>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/shop" element={<ShopPage />}>
+                <Route path=":category" element={<ShopPage />} />
+              </Route>
+              <Route path="/shop/cart" element={<CartPage />} />
+              <Route path="/shop/products/:productId" element={<ProductPage />} />
             </Route>
-            <Route path="/shop/products/:productId" element={<ProductPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="/sign-in" element={<SignInPage />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </ClerkProvider>
   </StrictMode>
 );
